7f1627125dd7ff161debcc0ea2edbd68459172f4
Alawpak <alan281@hotmail.es>
2023-05-06 22:49:47 -0600

    implementacion funciones extraccion tags

--- src/services/search-product/index.ts
+++ src/services/search-product/index.ts
@@ -1,11 +1,27 @@
 import axios from "axios";
+import {
+  extraerSepararPalabras,
+  filtrarPalabras,
+  eliminarPalabrasRepetidas,
+} from "commons/functions/helpers";
+import { colors, clothes } from "commons/constants/catalogs";
 
 const BASE_URL = "https://api.bing.microsoft.com/v7.0/images/visualsearch";
 
 export const postImageToVisualSearch = async (data: FormData) => {
   try {
     const response = await axios.post(BASE_URL, data);
-    if (response.status === 200 && response.data) return response.data;
+    if (response.status === 200 && response.data) {
+      const palabrasExtraidos = extraerSepararPalabras(response.data);
+      const palabrasFiltrados = filtrarPalabras(
+        palabrasExtraidos,
+        colors,
+        clothes
+      );
+      const tagsFinal = eliminarPalabrasRepetidas(palabrasFiltrados);
+      console.log(tagsFinal);
+      return response.data;
+    }
     return null;
   } catch (error) {
     console.error(error);
